8edad2977be31ebc246bd786587566a0dbe4278b
LaupWing <loc_strict@hotmail.com>
2022-12-08 15:01:45 +0100

    variants

--- src/components/Base.js
+++ src/components/Base.js
@@ -2,22 +2,30 @@ import React from "react"
 import { Link } from "react-router-dom"
 import { motion } from "framer-motion"
 
+const containerVariants = {
+   hidden: {
+      x: "100vw",
+      opacity: 0
+   },
+   visible: {
+      opacity: 1,
+      x: 0,
+      transition:{
+         type: "spring",
+         delay: 0.5
+      }
+   }
+}
+
 const Base = ({ addBase, pizza }) => {
    const bases = ["Classic", "Thin & Crispy", "Thick Crust"]
 
    return (
       <motion.div 
          className="base container"
-         initial={{
-            x: "100vw"
-         }}
-         animate={{
-            x: 0
-         }}
-         transition={{
-            type: "spring",
-            delay: 0.5
-         }}
+         variants={containerVariants}
+         initial="hidden"
+         animate="visible"
       >
          <h3>Step 1: Choose Your Base</h3>
          <ul>
