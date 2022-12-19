c16ee063d8853abc888308e7f654383e840ed744
LaupWing <loc_strict@hotmail.com>
2022-12-19 17:52:39 +0100

    order variants

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,14 +1,35 @@
 import React from "react"
+import {motion} from "framer-motion"
+
+const containerVariants = {
+   hidden: {
+      x: "100vw",
+      opacity: 0
+   },
+   visible: {
+      opacity: 1,
+      x: 0,
+      transition:{
+         type: "spring",
+         delay: 0.5
+      }
+   }
+}
 
 const Order = ({ pizza }) => {
    return (
-      <div className="container order">
+      <motion.div 
+         className="container order"
+         variants={containerVariants}
+         initial="hidden"
+         animate="visible"
+      >
          <h2>Thank you for your order :)</h2>
          <p>You ordered a {pizza.base} pizza with:</p>
          {pizza.toppings.map((topping) => (
             <div key={topping}>{topping}</div>
          ))}
-      </div>
+      </motion.div>
    )
 }
 
